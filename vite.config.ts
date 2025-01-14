f6eaf225328a9148c3c05d089a30238c0d2850f6
Wargas Teixeira <teixeira.wargas@gmail.com>
2025-01-14 13:16:15 -0300

    fix-base

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/sefin',
+  // base: '/sefin',
   plugins: [react()],
   build: {
     // outDir: '../wargas.github.io/sefin',
